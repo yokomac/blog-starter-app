647e32a4cc477ac8410041f3dd7eb4b6628b4ecd
yokomac <yokomaku.connecta@gmail.com>
2024-03-09 17:47:15 +0900

    First

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -11,14 +11,8 @@ export function Footer() {
           </h3>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts"
+              href="https://github.com/yokomac/blog-starter-app"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
             >
               View on GitHub
             </a>
